8e77931d8fecbb4dd0e7d44729a9da96fcd0edea
joaocastro <joaocastro0429@gmail.com>
2024-03-21 22:32:39 -0300

    add o route params

--- src/server.js
+++ src/server.js
@@ -5,6 +5,15 @@
 
  const app=express()
 
+ app.get("/api",(request,response)=>{
+    return response.json({message:"cadastro de produtos"})
+ })
+
+ app.get("/api/:id/:user",(request,response)=>{
+    const {id,user}=request.params
+    return response.json(`ID : ${id} O nome do produto é ${user}`)
+ })
 
 
- app.listen(process.env.PORT|| 8080)
+
+ app.listen(3333)
